395f5e2c39cb13f8d48421686573314f02bd7059
Misael <elmisamouse@gmail.com>
2025-12-26 17:42:29 -0600

    CHORE: Implementar .gitignore raíz y limpiar archivos rastreados innecesarios

--- backend/dist/routes/aiRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const aiController_1 = require("../controllers/aiController");
-const router = (0, express_1.Router)();
-router.post('/chat', aiController_1.chatWithAI);
-exports.default = router;
--- backend/dist/routes/lawyerRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const lawyerController_1 = require("../controllers/lawyerController");
-const router = (0, express_1.Router)();
-router.get('/', lawyerController_1.getLawyers);
-router.get('/:id', lawyerController_1.getLawyerById);
-router.post('/contact', lawyerController_1.contactLawyer);
-exports.default = router;
